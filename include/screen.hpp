bd0764b9c9e6ae3d9b1bc833fcc2f3f0583e6d5c
Lewis-Pitman <lpitman08@outlook.com>
2024-11-11 22:14:32 +0000

    Command + input implemented. Main view minor work
    Command view has been updated to support dynamic titles and descriptions. It also displays correctly.
    Input view was implemented, although still needs work to actually handle input later.
    Work has started on the main screen, however it is completely broken in regards to drawing colour.

--- include/screen.hpp
+++ include/screen.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <array>
+#include <vector>
 
 enum lineType {
 	//For readability
@@ -20,40 +21,55 @@ enum commandScreenType {
 
 enum consoleColour {
 	//For use in SetConsoleAttribute()
-	red = 41,
-	yellow = 43,
-	green = 42,
-	blue = 44,
-	white = 47,
-	black = 40
+	red = 0x40,
+	yellow = 0x60,
+	green = 0x20,
+	blue = 0x10,
+	white = 0x70,
+	black = 0x00
 };
-
-//List of usable commands
-extern const std::array<std::string, 3> rootCommands;
-extern const std::array<std::string, 4> fileCommands;
-extern const std::array<std::string, 4> toolCommands;
-extern const std::array<std::string, 7> colourCommands;
 
 class screen {
 public:
 	//View dimensions
-	const int padding{ 3 };
+	static const int padding{ 3 };
 	const std::pair<int, int> commandViewDimensions{ 20, 3 };
-	const std::pair<int, int> mainViewDimensions{ 20, 18 };
+	const std::pair<int, int> mainViewDimensions{ 50, 18 };
 	const std::pair<int, int> inputViewDimensions{ 20, 1 };
 
+private:
+	//List of available commands
+	const std::array<std::string, 3> rootCommands = { "file", "tool", "colour" };
+	const std::array<std::string, 4> fileCommands = { "back", "new", "open", "export" };
+	const std::array<std::string, 4> toolCommands = { "back", "brush", "line", "fill" };
+	const std::array<std::string, 7> colourCommands = { "back", "red", "yellow", "green", "blue", "white", "black" };
+
+	//A vector of colours, used for drawing the image
+	static std::vector<consoleColour> pixels;
+
 public:
-	//screen();
-	//~screen();
+	screen();
+	~screen();
 
 	void drawScreen();
 
-protected:
-	inline void drawLine(int width, int padding, lineType type);
-	inline int printCommands(commandScreenType type);
-
 private:
-	void drawCommandView();
+	//Misc-----------------------------------------------------
+	inline void drawLine(int width, int padding, lineType type);
+
+	//Command View---------------------------------------------
+	inline void printCommands(commandScreenType type);
+	inline void drawCommandView(std::string title, std::string description);
+	void drawCommandView(commandScreenType type);
+
+	//Input View-----------------------------------------------
+	void drawInputView();
+
+	//Main View------------------------------------------------
+	void drawAlert(std::string title, std::string description);
+	void drawMainView();
+	void resizeMainView(int width, int height);
+
 };
 
 #endif
--- src/screen.cpp
+++ src/screen.cpp
@@ -1,40 +1,18 @@
 #include <iostream>
 #include <iomanip>
 #include <array>
+#include <Windows.h>
+
 #include "../include/screen.hpp"
 
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); 
 
-extern const std::array<std::string, 3> rootCommands = { "file", "tool", "colour" };
-extern const std::array<std::string, 4> fileCommands = { "back", "new", "open", "export" };
-extern const std::array<std::string, 4> toolCommands = { "back", "brush", "line", "fill" };
-extern const std::array<std::string, 7> colourCommands = { "back", "red", "yellow", "green", "blue", "white", "black" };
-
-//Print an array and return the number of characters used. Necessary for printing boxes
-inline int screen::printCommands(commandScreenType type) { 
-	switch (type) {
-	case root:
-		for (int i = 0; i < rootCommands.size() - 1; i++) {
-			std::cout << rootCommands[i] << ", ";
-		}
-		break;
-	case file:
-		for (int i = 0; i < fileCommands.size() - 1; i++) {
-			std::cout << rootCommands[i] << ", ";
-		}
-		break;
-	case tool:
-		for (int i = 0; i < toolCommands.size() - 1; i++) {
-			std::cout << rootCommands[i] << ", ";
-		}
-		break;
-	case colour:
-		for (int i = 0; i < colourCommands.size() - 1; i++) {
-			std::cout << rootCommands[i] << ", ";
-		}
-		break;
-	}
-	return 0;
+screen::screen() { //constructor
+	static std::vector<consoleColour> pixels;
+	pixels.reserve(50 * 50); //Reserve the memory needed for the maximum size.
 }
+
+screen::~screen() {} //Deconstructor
 
 inline void screen::drawLine(int width, int padding, lineType type) {
 	if (type == dash) {
@@ -46,22 +24,90 @@ inline void screen::drawLine(int width, int padding, lineType type) {
 	else{
 		std::cout << "\n";
 		for (int i = 0; i < width; i++) {
-			std::cout << std::setw(padding) << i;
+			std::cout << std::setw(padding) << i + 1;
 		}
 	}
 	std::cout << "\n";
 }
 
-inline void screen::drawCommandView() {
+inline void screen::printCommands(commandScreenType type) { 
+
+	std::string commandString = "";
+
+	switch (type) {
+	case root:
+		for (int i = 0; i < rootCommands.size(); i++) {
+			commandString += rootCommands[i] + ", ";
+		}
+		break;
+	case file:
+		for (int i = 0; i < fileCommands.size(); i++) {
+			commandString += fileCommands[i] + ", ";
+		}
+		break;
+	case tool:
+		for (int i = 0; i < toolCommands.size(); i++) {
+			commandString += toolCommands[i] + ", ";
+		}
+		break;
+	case colour:
+		for (int i = 0; i < colourCommands.size(); i++) {
+			commandString += colourCommands[i] + ", ";
+		}
+		break;
+	}
+	
+	commandString[commandString.size() - 2] = ' '; //Remove the comma on the last command
+	std::cout << commandString << std::setw((commandViewDimensions.first * padding) - commandString.length()) << "|";
+
+}
+
+inline void screen::drawCommandView(commandScreenType type) {
 	drawLine(commandViewDimensions.first, padding, dash);
 	std::cout << "|Available commands:" << std::setw((commandViewDimensions.first * padding) - 19) << "|";
 	std::cout << "\n|";
-	printCommands(tool);
-
-
+	printCommands(type);
 	drawLine(commandViewDimensions.first, padding, dash);
 }
 
+inline void screen::drawCommandView(std::string title, std::string description) { //Overload for a custom title and description
+	drawLine(commandViewDimensions.first, padding, dash);
+	std::cout << "|";
+	std::cout << title << std::setw((commandViewDimensions.first * padding) - title.length()) << "|";
+	std::cout << "\n|";
+	std::cout << description << std::setw((commandViewDimensions.first * padding) - description.length()) << "|";
+	drawLine(commandViewDimensions.first, padding, dash);
+}
+
+void screen::drawMainView() {
+	int lineNumber = 0;
+
+	drawLine(mainViewDimensions.first, padding, number);
+	drawLine(mainViewDimensions.first, padding, dash);
+
+	for (int i = 0; i < mainViewDimensions.second; i++) {
+		std::cout << "\n|";
+		for (int j = 0; j < mainViewDimensions.first; j++) {
+			SetConsoleTextAttribute(hConsole, white);
+			//SetConsoleTextAttribute(hConsole, pixels[(lineNumber * mainViewDimensions.first) + j]); might work..? Using above for testing purposes
+			std::cout << std::setw((padding * 2) - 1);
+			std::cout << " ";
+		}
+		SetConsoleTextAttribute(hConsole, black);
+		std::cout << "| " << i + 1;
+
+		lineNumber++;
+	}
+}
+
+void screen::drawInputView() {
+	drawLine(inputViewDimensions.first, padding, dash);
+	std::cout << "|Input: " << std::setw((inputViewDimensions.first * padding) - 7) << "|";
+	drawLine(inputViewDimensions.first, padding, dash);
+}
+
 void screen::drawScreen() {
-	drawCommandView();
+	drawCommandView(file);
+	drawInputView();
+	drawMainView();
 }
